d9b3cd35ec1eb8aaa86c1801501b4538f7880058
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2021-08-23 22:25:52 -0500

    Este es el commit de la última clase

---
+++ app.js
@@ -0,0 +1,41 @@
+// app.js
+const $forms = document.querySelectorAll(".signup-form");
+
+const getTemplate = () => {
+  return fetch("./template.html").then((response) => response.text());
+};
+
+const sendEmailToApi = (address, template) => {
+  fetch(`https://bedu-email-sender-api.herokuapp.com/send`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: address,
+      template: template,
+    }),
+  })
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const sendEmail = (event) => {
+  event.preventDefault();
+  const email = event.target.querySelector("input").value;
+  getTemplate()
+    .then((template) => {
+      sendEmailToApi(email, template);
+    })
+    .catch((error) => {
+      console.log(error, "error al convertir el template.");
+    });
+};
+
+for (let i = 0; i < $forms.length; i++) {
+  $forms[i].addEventListener("submit", sendEmail);
+}
